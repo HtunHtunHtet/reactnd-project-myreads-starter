cf6044b04d60aab9bda4ccc66551b434ce342bf3
htunhtunhtet <ryanhhh91@gmail.com>
2018-02-04 23:29:58 +0800

    add react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 import ListCurrentlyReading from './ListCurrentlyReading'
 import ListWantToRead from './ListWantToRead'
 import ListRead from './ListRead'
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { Router } from 'react-router-dom'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+    <App />, document.getElementById('root')
+)
