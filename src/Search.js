bc53c5ab8499246e35a398313fc09070ae3a5223
htunhtunhtet <ryanhhh91@gmail.com>
2018-02-09 12:29:58 +0800

    more updates

--- src/Search.js
+++ src/Search.js
@@ -29,10 +29,10 @@ class Search extends Component {
     }
 
     render() {
-
         const { query, newBooks, searchErr} = this.state
         const { books, changeShelf } = this.props
 
+        console.log(this.state);
         return(
             <div className="search-books">
                 <div className="search-books-bar">
